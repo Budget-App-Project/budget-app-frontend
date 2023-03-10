aa468eec4e829e587634b334e150919a26290489
Timothy <timothy@Timothys-MacBook-Air.local>
2023-03-09 20:46:33 -0600

    Set up views for all pages and set up Angular canActive routing for protected routes once login and signup is set up.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddExpenseComponent } from './components/add-expense/add-expense.component';
+import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AuthGuard } from './services/auth.guard'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path:'addexpense', component: AddExpenseComponent, canActivate: [AuthGuard] },
+  { path: 'expenselist', component: ExpenseListComponent, canActivate: [AuthGuard] },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddExpenseComponent } from './components/add-expense/add-expense.component';
+import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthService } from './services/auth.service'
+import { AuthGuard } from './services/auth.guard'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddExpenseComponent,
+    ExpenseListComponent,
+    PageNotFoundComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
